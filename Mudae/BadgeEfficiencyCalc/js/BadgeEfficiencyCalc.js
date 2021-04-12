4b0aed699bb9aa807d3fd588f700f2b7ab684381
Sverrir Hákon <24295046+Svessinn@users.noreply.github.com>
2021-04-12 20:53:58 +0000

    Update BadgeEfficiencyCalc.js

--- Mudae/BadgeEfficiencyCalc/js/BadgeEfficiencyCalc.js
+++ Mudae/BadgeEfficiencyCalc/js/BadgeEfficiencyCalc.js
@@ -15,7 +15,7 @@ Vue.component("value-input", {
                       <label :for="id">{{label}}</label>
                       <div class="input-group">
                         <div class="input-group-prepend">
-                          <span class="input-group-text"><i :class="'fas fa-' + faIcon" style="color:#FB4699;"></i></span>
+                          <span class="input-group-text"><i :class="'fas fa-' + faIcon"></i></span>
                         </div>
                         <input type="number" class="form-control" id="claimRank" :placeholder="label" :value="value" @input="updateValue" :min="minTick" pattern="\d+" required>
                       </div>
