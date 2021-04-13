4f900b04a7816a796c83b299767e7995503a3997
Sverrir Hákon <24295046+Svessinn@users.noreply.github.com>
2021-04-13 02:42:42 +0000

    Update BadgeEfficiencyCalc.js

--- Mudae/BadgeEfficiencyCalc/js/BadgeEfficiencyCalc.js
+++ Mudae/BadgeEfficiencyCalc/js/BadgeEfficiencyCalc.js
@@ -380,7 +380,7 @@ let app = new Vue({
 			let remaining = this.EmeraldRouteMid[52];
 			let time = Math.round(remaining/this.EmeraldRouteIncome[2]*100)/100;
 			let lst = [remaining, time];
-			lst.push(Math.round((this.EmeraldRouteMid[54]+time)*100)/100, Math.round((this.EmeraldRouteMid[55]+time)*100)/100);
+			lst.push(Math.round((this.EmeraldRouteMid[54]+time)*100)/100, Math.round((this.EmeraldRouteMid[55]+time)*100)/100, Math.round((this.EmeraldRouteMid[56]+time)*100)/100, Math.round((this.EmeraldRouteMid[57]+time)*100)/100, Math.round((this.EmeraldRouteMid[58]+time)*100)/100, Math.round((this.EmeraldRouteMid[59]+time)*100)/100);
 			return lst;
 		}
 	},
