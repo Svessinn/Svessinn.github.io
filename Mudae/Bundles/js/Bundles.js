c09006a559b88d8b4a193271dc4f58117402e653
Sverrir Hákon <24295046+Svessinn@users.noreply.github.com>
2021-04-21 20:57:49 +0000

    Create Bundles.js

---
+++ Mudae/Bundles/js/Bundles.js
@@ -0,0 +1,48 @@
+const app = new Vue({
+  el:'#app',
+  data:{
+    bundles:[],
+    currentSort:'Size',
+    currentSortDir:'asc',
+    pageSize:10,
+    currentPage:1
+  },
+  created:function() {
+    fetch('https://svessinn.github.io/Mudae/Bundles/Bundles.json')
+    .then(res => res.json())
+    .then(res => {
+      this.bundles = res;
+    })
+  },
+  methods:{
+    sort:function(s) {
+      //if s == current sort, reverse
+      if(s === this.currentSort) {
+        this.currentSortDir = this.currentSortDir==='asc'?'desc':'asc';
+      }
+      this.currentSort = s;
+    },
+    nextPage:function() {
+      if((this.currentPage*this.pageSize) < this.bundles.length) this.currentPage++;
+    },
+    prevPage:function() {
+      if(this.currentPage > 1) this.currentPage--;
+    }
+
+  },
+  computed:{
+    sortedBundles:function() {
+      return this.bundles.sort((a,b) => {
+        let modifier = 1;
+        if(this.currentSortDir === 'desc') modifier = -1;
+        if(a[this.currentSort] < b[this.currentSort]) return -1 * modifier;
+        if(a[this.currentSort] > b[this.currentSort]) return 1 * modifier;
+        return 0;
+      }).filter((row, index) => {
+        let start = (this.currentPage-1)*this.pageSize;
+        let end = this.currentPage*this.pageSize;
+        if(index >= start && index < end) return true;
+      });
+    }
+  }
+})
