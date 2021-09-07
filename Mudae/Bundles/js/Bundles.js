93e2538408a9ab00aac89ca57e09a82b8cba41c8
Sverrir Hákon <24295046+Svessinn@users.noreply.github.com>
2021-09-07 17:53:36 +0000

    Update Bundles.js

--- Mudae/Bundles/js/Bundles.js
+++ Mudae/Bundles/js/Bundles.js
@@ -26,8 +26,8 @@ const app = new Vue({
       return this.bundles.sort((a, b) => {
         let modifier = 1;
         if (this.currentSortDir === 'desc') modifier = -1;
-        if (a[this.currentSort].toLowerCase() < b[this.currentSort].toLowerCase()) return -1 * modifier;
-        if (a[this.currentSort].toLowerCase() > b[this.currentSort].toLowerCase()) return 1 * modifier;
+        if (a[this.currentSort] < b[this.currentSort]) return -1 * modifier;
+        if (a[this.currentSort] > b[this.currentSort]) return 1 * modifier;
         return 0;
       });
     }
